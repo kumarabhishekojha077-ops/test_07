42a926e9fcede0d1e5afc692cf3f899e65466174
kumarabhishekojha077-ops <kumarabhishekojha077@gmail.com>
2026-05-29 00:12:08 +0530

    Update student.java

--- student.java
+++ student.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class StudentTest {
+class student {
     @Test
     void testGreet() {
         Student s = new Student();
